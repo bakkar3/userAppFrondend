3babce713336ca455756743cf33783c53e5fcc66
Ammar Bakkar <amarbakar33@gmail.com>
2021-11-16 15:29:01 +0100

    fertig

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import "./App.scss";
+import { GrEdit } from 'react-icons/gr';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:3016");
+      const users = await response.json();
+      setUsers(users);
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Edit User App Frontend</h1>
+      <div className="topRow">
+
+      <button >add user</button>
+      </div>
+      <section className="users">
+      {users.map((user, index) => {
+        return (
+            <div key={index} className="userCard">
+                <div className="row">
+                    <div className="label">Full Name:</div>
+                    <div className="data">{user.name}</div>
+                </div>
+                <div className="row">
+                    <div className="label">User Name:</div>
+                    <div className="data">{user.username}</div>
+                </div>
+                <div className="row">
+                    <div className="label">E-Mail:</div>
+                    <div className="data">{user.email}</div>
+                </div>
+                <div className="iconRow">
+    <button className="icon"><RiDeleteBin6Line/></button>
+    <button className="icon"><GrEdit /></button>
+</div>
+            </div>
+        )
+    })}
+      </section>
+    </div>
+  );
+}
+
+export default App;
